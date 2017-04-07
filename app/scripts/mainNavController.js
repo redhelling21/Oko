2ded49aae9bac8ec658eb054ce1445a59783c651
redhelling21 <theo.buri@utbm.fr>
2017-04-08 01:36:00 +0200

    First try of modal

--- app/scripts/mainNavController.js
+++ app/scripts/mainNavController.js
@@ -1,4 +1,17 @@
 angApp.
-controller('MainNavCtrl', function($scope){
-	$scope.title = "Oko";
+controller('MainNavCtrl', function($scope, $mdDialog){
+	$scope.showSettings = function(ev) {
+        $mdDialog.show({
+            controller: 'SettingsCtrl',
+          templateUrl: 'views/settings.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose:false
+  })
+        .then(function(answer) {
+          $scope.status = 'You said the information was "' + answer + '".';
+      }, function() {
+          $scope.status = 'You cancelled the dialog.';
+      });
+    };
 });
---
+++ app/scripts/settingsController.js
@@ -0,0 +1,14 @@
+angApp.
+controller('SettingsCtrl', function($scope, $mdDialog){
+	$scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+});

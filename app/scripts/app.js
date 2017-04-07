9278e24e5f232d9f13d72a2892f95227999ece09
thburi <theo.buri@capgemini.com>
2017-04-07 17:06:21 +0200

    Applied dark theme, custom color svg icons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,7 @@
 var angApp = angular.module('myApp', ['ngMaterial']);
+angApp.config(function($mdThemingProvider){
+    $mdThemingProvider.theme('default')
+    .primaryPalette('blue')
+    .accentPalette('orange')
+    .dark();
+});

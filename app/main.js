6624009f0e9ec096e1437eb2470db5ec42f4a446
redhelling21 <theo.buri@utbm.fr>
2017-04-12 19:17:51 +0200

    Async file loading

--- app/main.js
+++ app/main.js
@@ -3,7 +3,9 @@ const electron = require('electron');
 const {ipcMain} = require('electron')
 const app = electron.app;
 const remote = electron.remote;
-const dialog = electron.dialog
+const dialog = electron.dialog;
+const fs = require('fs');
+
 var child;
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
@@ -56,13 +58,35 @@ exports.selectDirectory = function() {
 }
 
 var scanFolders = function(dir, event) {
-    var args = [dir];
-    child = require('child_process').fork(`app/scripts/folderScanner.js`, args);
-    child.on('message', function(message) {
-        event.sender.send('scan-folders-reply', message);
-    });
+    var pathList = [];
+    console.log("Dir : " + dir);
+    pathList = walkSync(dir, pathList);
+    event.sender.send('scan-folders-reply', pathList);
 }
 
 ipcMain.on('scan-folders', (event, arg) => {
     scanFolders(arg, event);
 })
+
+var walkSync = function(dir, filelist) {
+        try {
+            console.log(dir);
+            var fs = fs || require('fs'),
+            files = fs.readdirSync(dir);
+            filelist = filelist || [];
+            files.forEach(function(file) {
+                if (fs.statSync(dir + '/' + file).isDirectory()) {
+                    filelist = walkSync(dir + '/' + file, filelist);
+                }
+                else {
+                    if(['png', 'jpg', 'jpeg'].indexOf(file.split('.').pop()) !== -1){
+                        filelist.push(dir + '/' + file);
+                    }
+                }
+            });
+        }catch(err) {
+            console.log(err.message);
+        }finally {
+            return filelist;
+        }
+    };

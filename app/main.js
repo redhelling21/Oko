26c12b15c81422dec6a18d2f4beb7d9da23fa97d
redhelling21 <theo.buri@utbm.fr>
2017-04-13 09:30:53 +0200

    Async images loading

--- app/main.js
+++ app/main.js
@@ -55,14 +55,34 @@ exports.selectDirectory = function() {
     })
 }
 
-var scanFolders = function(dir, event) {
-    var args = [dir];
-    child = require('child_process').fork(`app/scripts/folderScanner.js`, args);
-    child.on('message', function(message) {
-        event.sender.send('scan-folders-reply', message);
-    });
+var scanFolders = async function(dir, event) {
+    var pathList = [];
+    pathList = walkSync(dir, pathList);
+    event.sender.send('scan-folders-reply', pathList);
 }
 
 ipcMain.on('scan-folders', (event, arg) => {
     scanFolders(arg, event);
 })
+
+var walkSync = function(dir, filelist) {
+        try {
+            var fs = fs || require('fs'),
+            files = fs.readdirSync(dir);
+            filelist = filelist || [];
+            files.forEach(function(file) {
+                if (fs.statSync(dir + '/' + file).isDirectory()) {
+                    filelist = walkSync(dir + '/' + file, filelist);
+                }
+                else {
+                    if(['png', 'jpg', 'jpeg'].indexOf(file.split('.').pop()) !== -1){
+                        filelist.push(dir + '/' + file);
+                    }
+                }
+            });
+        }catch(err) {
+            console.log(err.message);
+        }finally {
+            return filelist;
+        }
+    };

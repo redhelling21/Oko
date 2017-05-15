b5a8280104ecebe0204d6df414dddb4fe83bdf67
thburi <theo.buri@capgemini.com>
2017-05-15 15:17:42 +0200

    Filter by tags display improvements

--- app/scripts/galleryController.js
+++ app/scripts/galleryController.js
@@ -8,6 +8,31 @@ controller('GalleryCtrl', ['$scope', 'angularGridInstance', '$mdToast', '$timeou
     $scope.page = 0;
     var fullyLoaded = false;
 
+
+    $scope.filterTags = [];
+
+    $scope.addFilterTag = function(tag){
+        console.log("tag : " + tag);
+        var index = $scope.filterTags.indexOf(tag);
+        console.log("index : " + index);
+        if(index === -1){
+            $scope.filterTags = $scope.filterTags.concat(tag);
+        }else{
+            $scope.filterTags.splice(index);
+        }
+        console.log("filtertag : " + $scope.filterTags);
+    };
+
+    $scope.tagButtonColor = function(tag){
+        console.log("Style updated");
+        if($scope.filterTags.indexOf(tag) !== -1){
+            return {"background-color": "blue"};
+        }else{
+            return {"background-color": "green"};
+        }
+    }
+
+
     ipcRenderer.on('scan-folders-reply', function(event, arg){
         var images = arg, temp = [], i = 0, hasTags, hasStars, hasGeo, hasMetas, image;
         $scope.$parent.existingTags.clear();
